509c770734da76a379bdf7de079f200fdc6718ea
Admin <Admin@DESKTOP-SRMBPCR>
2026-05-18 12:33:45 +0530

    Add project files.

---
+++ Protected AccessModifier/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protected_AccessModifier
+{
+    class MyClass
+    {
+        protected string Name;
+        public MyClass(string name)
+        {
+            Name = name;
+            Console.WriteLine("Name is :" + Name);
+        }
+    }
+    class NewClass:MyClass
+    {
+        public NewClass(string name):base(name)
+        {
+            Name = name;
+            Console.WriteLine("Name is :"+Name);
+        }
+    }
+   
+    internal class Program:NewClass
+    {
+        public Program(string name) : base(name)
+        {
+            Name = name;
+            Console.WriteLine("Name is :" + Name);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program("Rani");
+        }
+    }
+}
