4cc334c7fed38635d4768bff857860c62ffb2e40
Admin <Admin@DESKTOP-SRMBPCR>
2026-05-25 16:29:25 +0530

    changes

---
+++ accessProtected/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accessProtected
+{
+    class student
+    {
+        protected int id;
+        public string name;
+        public int age;
+        public student(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            Console.WriteLine("Student id is :" + id);
+            Console.WriteLine("Student name is :" + name);
+            Console.WriteLine("Student age is :" + age);
+        }
+        
+    }
+    class student2:student
+    {
+        string course;
+        public student2(int id,string name,int age,string course):base(id,name,age)
+        {
+            this.course = course;
+            Console.WriteLine("Student Course : "+course);
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student id : ");
+            int id=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter student name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter student age : ");
+            int age = int.Parse(Console.ReadLine());
+
+            student2 std = new student2(id,name,age,"CSC");
+            
+        }
+    }
+}
---
+++ protected/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @protected
+{
+    class student
+    {
+        protected int sid;
+        protected string college;
+        public student(int sid,string college)
+        {
+            this.sid = sid;
+            this.college = college;
+        }
+        public void display()
+        {
+            Console.WriteLine("Student id : " + sid);
+            Console.WriteLine("Student College : "+college);
+        }
+    }
+    class student1:student
+    {
+        string sname;
+        public student1(string sname):base(12,"raja")
+        {
+            this.sname=sname;
+        }
+        public void display1()
+        {
+            Console.WriteLine("Student name : "+sname);
+            base.display();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            student1 s = new student1("Ravi");
+            s.display1();
+        }
+    }
+}
